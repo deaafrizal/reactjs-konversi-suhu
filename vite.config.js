a53270d12d313c1b7972a96ffcca676c4162f9b7
deaaprizal <deascript@icloud.com>
2025-07-10 20:57:22 +0700

    fix vite config for staging

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-  host: '0.0.0.0',
+    host: '0.0.0.0',
     port: 3000,
-    strictPort: true
+    strictPort: true,
+    allowedHosts: [
+      'frontend-service.reactjstemp.svc.cluster.local',
+      '194.238.16.49'
+    ]
+  },
+  preview: {
+    https: false,
+    strictPort: true,
+    open: true
   }
 })
