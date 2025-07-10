fa12d9a9f5b54b5cdc4f5d9e1e33d6f4a241c31f
deaaprizal <deascript@icloud.com>
2025-07-10 21:11:15 +0700

    fix vite config for staging

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,15 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
-    allowedHosts: [
-      'frontend-service.reactjstemp.svc.cluster.local',
-      '194.238.16.49'
-    ]
+    // allowedHosts: [
+    //   'frontend-service.reactjstemp.svc.cluster.local',
+    //   '194.238.16.49'
+    // ]
   },
   preview: {
     https: false,
     strictPort: true,
+    port: 3000,
     open: true
   }
 })
